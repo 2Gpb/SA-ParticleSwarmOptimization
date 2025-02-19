29f6a64d3b1a848f137c5577cc0050a46bdc778c
2Gpb <Pvtyukhmenev@gmail.com>
2025-02-19 16:10:07 +0300

    add adaptive parameters

--- SA-ParticleSwarmOptimization/__main__.py
+++ SA-ParticleSwarmOptimization/__main__.py
@@ -3,25 +3,36 @@ from particle import *
 
 
 class ParticleSwarmOptimization:
-    def __init__(self, cost_func, dim=2, num_particles=30, max_iter=100, w=0.9, c1=1.5, c2=1.5, min_bound=-5.12,
-                 max_bound=5.12):
+    def __init__(self, cost_func, dim=2, num_particles=30, max_iter=100, w_max=0.9, w_min=0.4, c1=1.5, c2=1.5,
+                 min_bound=-5.12, max_bound=5.12, adaptive_w=False):
         self.cost_func = cost_func
         self.dim = dim
         self.num_particles = num_particles
         self.max_iter = max_iter
-        self.w = w
+        self.w_max = w_max
+        self.w_min = w_min
         self.c1 = c1
         self.c2 = c2
         self.bounds = SimpleNamespace(min=min_bound, max=max_bound)
         self.swarm = [Particle(self.dim, self.bounds) for _ in range(self.num_particles)]
         self.swarm_best_position = None
         self.swarm_best_fitness = np.inf
+        self.adaptive_w = adaptive_w
 
-    def __update_velocities(self):
+    def _update_inertia_weight(self, iter_num):
+        if self.adaptive_w:
+            size = self.num_particles
+            dimension = self.dim
+            fitness_value = self.swarm_best_fitness
+            return (3 - np.exp(-size / 200) + (fitness_value / (8 * dimension)) ** 2) ** -1 + 0.8
+        else:
+            return self.w_max - ((self.w_max - self.w_min) * iter_num) / self.max_iter
+
+    def __update_velocities(self, w):
         for particle in self.swarm:
             r1 = np.random.uniform(0, 1, self.dim)
             r2 = np.random.uniform(0, 1, self.dim)
-            inertia = self.w * particle.velocity
+            inertia = w * particle.velocity
             cognitive = self.c1 * r1 * (particle.best_position - particle.position)
             social = self.c2 * r2 * (self.swarm_best_position - particle.position)
             particle.velocity = inertia + cognitive + social
@@ -44,12 +55,13 @@ class ParticleSwarmOptimization:
         for particle in self.swarm:
             particle.update_best(self.cost_func)
 
-        best_particle = min(self.swarm, key=lambda part: particle.best_fitness)
+        best_particle = min(self.swarm, key=lambda part: part.best_fitness)
         self.swarm_best_position = best_particle.best_position.copy()
         self.swarm_best_fitness = best_particle.best_fitness
 
-        for _ in range(self.max_iter):
-            self.__update_velocities()
+        for iter_num in range(self.max_iter):
+            w = self._update_inertia_weight(iter_num)
+            self.__update_velocities(w)
             self.__update_positions()
             self.__update_best_positions()
             self.__update_global_best()
@@ -61,26 +73,14 @@ def f1(sol):
     return np.sum(np.array(sol)**2)
 
 
-dimension1 = 10
-num_particles1 = 50
-max_iter1 = 1000
-w1 = 0.9
-c11 = 1.5
-c21 = 1.5
-min_bound1 = -5.12
-max_bound1 = 5.12
-
-
 pso = ParticleSwarmOptimization(
     cost_func=f1,
-    dim=dimension1,
-    num_particles=num_particles1,
-    max_iter=max_iter1,
-    w=w1,
-    c1=c11,
-    c2=c21,
-    min_bound=min_bound1,
-    max_bound=max_bound1
+    dim=3,
+    num_particles=30,
+    max_iter=1000,
+    min_bound=-5.12,
+    max_bound=5.12,
+    adaptive_w=True
 )
 
 solution, fitness = pso.optimize()
